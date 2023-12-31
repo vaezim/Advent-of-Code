a8a303ff1636053c193d1fe1ff36fe4c44b25fae
Max Remzi <mehradvaezi8@gmail.com>
2023-12-31 01:56:35 -0500

    Added 2017/day17

--- 2017/day17/utils.py
+++ 2017/day17/utils.py
@@ -4,14 +4,24 @@ class SpinLock:
 
     def CreateBuffer1(self):
         buffer = [0]
-        for curr in range(1,2017+1):
-            i = (buffer.index(curr-1) + self.step) % len(buffer)
-            buffer.insert(i+1, curr)
+        for curr in range(1, 2017 + 1):
+            i = (buffer.index(curr - 1) + self.step) % len(buffer)
+            buffer.insert(i + 1, curr)
         self.buffer = buffer
-        return buffer[buffer.index(len(buffer)-1)+1]
-    
+        return buffer[buffer.index(len(buffer) - 1) + 1]
+
     def CreateBuffer2(self):
-        pass
+        index_one_val = 0
+        curr = [0, 0]  # [val, index]
+        arr_size = 1
+        for _ in range(50_000_000):
+            curr_index = curr[1]
+            next_index = ((curr_index + self.step) % arr_size) + 1
+            if next_index == 1:
+                index_one_val = arr_size
+            curr = [arr_size, next_index]
+            arr_size += 1
+        return index_one_val
 
 
 if __name__ == "__main__":
