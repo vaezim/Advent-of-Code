281017886646756732753f42f7c3c40ea23b20c6
Max Remzi <91099081+vaezim@users.noreply.github.com>
2024-01-07 19:17:32 -0500

    Added 2018/day06

--- 2018/day06/main.py
+++ 2018/day06/main.py
@@ -1,57 +1,14 @@
-from utils import ManhattanDist
+from utils import Manhattan
 
 
-with open('input', 'r+') as file:
+with open("input", "r+") as file:
     lines = file.readlines()
-points = []
-for line in lines:
-    point = (int(line.split(',')[0]),
-             int(line.split(',')[1]))
-    points.append(point)
 
 ##### Part 1 #####
-grid_width = 400
-grid1 = [[[-1,float("inf")]]*grid_width for _ in range(grid_width)]
-grid2 = [[[-1,float("inf")]]*(grid_width+2) for _ in range(grid_width+2)]
-
-for i in range(len(grid1)):
-    for j in range(len(grid1[0])):
-        for p, point in enumerate(points):
-            manhattan_dist = ManhattanDist((i,j),point)
-            if manhattan_dist == grid1[i][j][1]:
-                break
-            if manhattan_dist < grid1[i][j][1]:
-                grid1[i][j][1] = manhattan_dist
-                grid1[i][j][0] = p
-
-for i in range(len(grid2)):
-    for j in range(len(grid2[0])):
-        for p, point in enumerate(points):
-            manhattan_dist = ManhattanDist((i-1,j-1),point)
-            if manhattan_dist == grid2[i][j][1]:
-                break
-            if manhattan_dist < grid2[i][j][1]:
-                grid2[i][j][1] = manhattan_dist
-                grid2[i][j][0] = p
-
-distances1 = [0]*len(points)
-distances2 = [0]*len(points)
-for i in range(len(grid1)):
-    for j in range(len(grid1[0])):
-        if grid1[i][j][0] != -1:
-            distances1[grid1[i][j][0]] += 1
-for i in range(len(grid2)):
-    for j in range(len(grid2[0])):
-        if grid2[i][j][0] != -1:
-            distances2[grid2[i][j][0]] += 1
-
-result = 0
-for i in range(len(points)):
-    if distances1[i] == distances2[i]:
-        result = max(result, distances1[i])
-
+man = Manhattan(lines)
+result = man.GetLargestArea()
 print(f"Answer of part 1: {result}")
 
 ##### Part 2 #####
-
+result = man.GetRegionSize()
 print(f"Answer of part 2: {result}")
--- 2018/day06/utils.py
+++ 2018/day06/utils.py
@@ -1,3 +1,73 @@
+from collections import defaultdict
 
-def ManhattanDist(p1: tuple, p2: tuple):
-    return abs(p1[0]-p2[0]) + abs(p1[1]-p2[1])
+
+class Manhattan:
+    def __init__(self, text):
+        self.points = self._Process(text)
+        self.width, self.height = self._GetGridSize()
+
+    def GetRegionSize(self):
+        region_size = 0
+        for i in range(self.width):
+            for j in range(self.height):
+                if self._SumDists((i,j)) < 10000:
+                    region_size += 1
+        return region_size
+
+    def _SumDists(self, p):
+        sum_dist = 0
+        for point in self.points:
+            sum_dist += self._Dist(p,point)
+        return sum_dist
+
+    def GetLargestArea(self):
+        self.area_map1 = defaultdict(lambda : 1)  # point => area_size
+        self.area_map2 = defaultdict(lambda : 1)  # point => area_size
+        self._GetAreaMapInGrid((0,0,self.width,self.height), self.area_map1)
+        self._GetAreaMapInGrid((-1,-1,self.width+1,self.height+1), self.area_map2)
+        largest_area = 0
+        for point in self.points:
+            if self.area_map1[point] != self.area_map2[point]:
+                continue
+            largest_area = max(largest_area, self.area_map1[point])
+        return largest_area
+
+    def _GetAreaMapInGrid(self, grid: tuple, area_map: map):
+        for i in range(grid[0], grid[2]):
+            for j in range(grid[1], grid[3]):
+                if (i,j) in self.points:
+                    continue
+                point = self._GetNearestPointTo((i,j))
+                if point != None:
+                    area_map[point] += 1
+    
+    def _GetNearestPointTo(self, p: tuple):
+        nearest = []
+        nearest_dist = float("inf")
+        for point in self.points:
+            dist = self._Dist(point, p)
+            if dist == nearest_dist:
+                nearest.append(point)
+            if dist < nearest_dist:
+                nearest_dist = dist
+                nearest.clear()
+                nearest.append(point)
+        if len(nearest) == 1:
+            return nearest[0]
+        return None
+    
+    def _Dist(self, p1, p2):
+        return abs(p1[0]-p2[0]) + abs(p1[1]-p2[1])
+
+    def _GetGridSize(self):
+        width, height = 0, 0
+        for point in self.points:
+            width = max(width, point[0])
+            height = max(height, point[1])
+        return (width+1, height+1)
+
+    def _Process(self, text):
+        points = []
+        for line in text:
+            points.append((int(line.split(',')[0]), int(line.split(',')[1])))
+        return set(points)
