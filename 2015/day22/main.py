c89a218a8f38fed2a5508922413997ab002f3997
Max Remzi <91099081+vaezim@users.noreply.github.com>
2024-01-12 22:01:12 -0500

    Added 2015/day22

--- 2015/day22/main.py
+++ 2015/day22/main.py
@@ -1,11 +1,9 @@
-
-with open('input', 'r+') as file:
-    lines = file.readlines()
+from utils import solve
 
 ##### Part 1 #####
-
+result = solve(part_two=False)
 print(f"[+] Answer of part 1: {result}")
 
 ##### Part 2 #####
-
+result = solve(part_two=True)
 print(f"[+] Answer of part 2: {result}")
