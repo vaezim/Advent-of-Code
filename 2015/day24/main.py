5ee85e1d7c9a7be191582a2203f8c257b88022c3
Max Remzi <91099081+vaezim@users.noreply.github.com>
2024-01-13 14:34:44 -0500

    Added 2015/day24,25

--- 2015/day24/main.py
+++ 2015/day24/main.py
@@ -1,15 +1,15 @@
 from utils import Santa
 
 
-with open('input', 'r+') as file:
+with open("input", "r+") as file:
     lines = file.readlines()
 
 ##### Part 1 #####
-packages = list(map(lambda x: int(x), lines))
-santa = Santa(packages)
+santa = Santa(list(map(lambda x: int(x), lines)), num_groups=3)
 result = santa.GetIdealConfigQE()
 print(f"[+] Answer of part 1: {result}")
 
 ##### Part 2 #####
-
+santa = Santa(list(map(lambda x: int(x), lines)), num_groups=4)
+result = santa.GetIdealConfigQE()
 print(f"[+] Answer of part 2: {result}")
--- 2015/day24/utils.py
+++ 2015/day24/utils.py
@@ -1,78 +1,86 @@
-
 class Santa:
-    def __init__(self, packages):
+    def __init__(self, packages, num_groups):
         self.packages = packages
-        self.weight = sum(self.packages) // 3
+        self.weight = sum(self.packages) // num_groups
         self.packages.sort()
-
-        self.curr_subset = []
-        self.weightSubsets = set()
+        self.all_subsets = set()
+        self.ideal_subsets = set()
 
     def GetIdealConfigQE(self):
-        partitions = self._GetPartitions()
-        minPartitionLen = float("inf")
-        minQE = float("inf")
-        for part in partitions:
-            minPart = min(part, key=lambda x: len(x))
-            if len(minPart) <= minPartitionLen:
-                minPartitionLen = len(minPart)
-                minQE = min(minQE, self._Product(minPart))
-        return minQE
+        self._GetAllSubsets()
+        self._GetIdealSubsets()
+        min_QE = float("inf")
+        min_size = len(min(self.ideal_subsets, key=lambda x: len(x)))
+        for ss in self.ideal_subsets:
+            if len(ss) == min_size:
+                min_QE = min(min_QE, self._QE(ss))
+        return min_QE
 
-    def _GetPartitions(self):
-        partitions = set()
-        if len(self.weightSubsets) == 0:
-            print("[*] Generating Candidate Subsets for Partitions...")
-            self._GeneratePartitionCandidates()
-        self.weightSubsets = list(self.weightSubsets)
-        minPartitionLen = float("inf")
-        print("[*] Generating Partitions...")
-        for i in range(len(self.weightSubsets)):
-            for j in range(1,len(self.weightSubsets)):
-                for k in range(2,len(self.weightSubsets)):
-                    currMinPartitionLen = min(len(self.weightSubsets[i]),len(self.weightSubsets[j]),len(self.weightSubsets[k]))
-                    if currMinPartitionLen <= minPartitionLen:
-                        if self._IsPartition(self.weightSubsets[i],self.weightSubsets[j],self.weightSubsets[k]):
-                            partitions.add((self.weightSubsets[i],self.weightSubsets[j],self.weightSubsets[k]))
-                            minPartitionLen = currMinPartitionLen
-        return partitions
+    def _GetIdealSubsets(self):
+        print("[*] Finding Partitions...")
+        for i, ss in enumerate(self.all_subsets):
+            if i % 40_000 == 0:
+                percentage = "{:.2f}".format(i / len(self.all_subsets) * 100)
+                print(f"{percentage}%")
+            ss_prime = self._SubtractSet(ss)
+            if self._IsPartitionableIn2(ss_prime):
+                self.ideal_subsets.add(ss)
 
-    def _GeneratePartitionCandidates(self, last=0):
-        S = sum(self.curr_subset)
-        if S == self.weight:
-            self.weightSubsets.add(tuple(sorted(self.curr_subset)))
-            return
-        if S > self.weight:
-            return -1
-        if last >= len(self.packages): return
-        for i in range(last,len(self.packages)):
-            # Don't add packages[last]
-            self._GeneratePartitionCandidates(i+1)
-            # Add packages[last]
-            self.curr_subset.append(self.packages[i])
-            ret = self._GeneratePartitionCandidates(i+1)
-            self.curr_subset.pop()
-            if ret == -1: return # don't check larger numbers
+    def _IsPartitionableIn2(self, s):
+        def _Recursion(curr_index, curr_sum):
+            if curr_sum == self.weight: return True
+            if curr_sum > self.weight: return False
+            if curr_index == len(s): return False
+            if _Recursion(curr_index+1, curr_sum+s[curr_index]):
+                return True
+            if _Recursion(curr_index+1, curr_sum):
+                return True
+            return False
+        return _Recursion(0, 0)
 
-    def _IsPartition(self, subset1, subset2, subset3):
+    def _SubtractSet(self, s):
+        init_set = set(self.packages.copy())
+        for element in s:
+            init_set.remove(element)
+        return list(init_set)
+
+    def _IsPartition(self, i, j, k):
         S = set()
-        for i in subset1:
-            S.add(i)
-        for i in subset2:
-            S.add(i)
-        for i in subset3:
-            S.add(i)
+        for element in i:
+            S.add(element)
+        for element in j:
+            S.add(element)
+        for element in k:
+            S.add(element)
         return len(S) == len(self.packages)
 
-    def _Product(self, nums):
-        p = 1
-        for i in nums:
-            p *= i
-        return p
+    def _GetAllSubsets(self):
+        print("[*] Generating all subsets...")
+        self._GetAllSubsetsRecursive(0, [], 0)
+
+    def _GetAllSubsetsRecursive(self, curr_index, curr_package_list, curr_sum):
+        if curr_sum == self.weight:
+            self.all_subsets.add(tuple(curr_package_list))
+            return
+        if curr_sum > self.weight:
+            return
+        if curr_index == len(self.packages):
+            return
+        # With      packages[curr_index]
+        curr_package_list.append(self.packages[curr_index])
+        self._GetAllSubsetsRecursive(
+            curr_index + 1, curr_package_list, curr_sum + self.packages[curr_index])
+        # Without   packages[curr_index]
+        curr_package_list.pop()
+        self._GetAllSubsetsRecursive(curr_index + 1, curr_package_list, curr_sum)
+
+    def _QE(self, subset):
+        i = 1
+        for item in subset:
+            i *= item
+        return i
 
 
 if __name__ == "__main__":
-    # packages = [1,2,3]
-    packages = [1,2,3,4,5,7,8,9,10,11]
-    santa = Santa(packages)
+    santa = Santa([1, 2, 3, 4, 5, 7, 8, 9, 10, 11])
     print(santa.GetIdealConfigQE())
