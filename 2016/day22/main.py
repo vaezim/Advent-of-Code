624c3f583c11de0bbe5915642b844374cad2449b
Max Remzi <91099081+vaezim@users.noreply.github.com>
2024-01-15 20:33:51 -0500

    Added 2016/day22

--- 2016/day22/main.py
+++ 2016/day22/main.py
@@ -1,7 +1,7 @@
 from utils import FileSystem
 
 
-with open('input', 'r+') as file:
+with open("input", "r+") as file:
     lines = file.readlines()
 
 ##### Part 1 #####
@@ -10,5 +10,6 @@ result = fs.GetNumViablePairs()
 print(f"[+] Answer of part 1: {result}")
 
 ##### Part 2 #####
-
+fs.DispToFile("filesystem.fs")
+result = 256  # Do it by hand !!! #
 print(f"[+] Answer of part 2: {result}")
--- 2016/day22/utils.py
+++ 2016/day22/utils.py
@@ -3,6 +3,8 @@ import re
 
 class FileSystem:
     def __init__(self, nodes_info):
+        self.width = 0
+        self.height = 0
         self.nodes = {}  # (x,y) --> [Size, Used, Avail, Use%]
         self._ProcessNodesInfo(nodes_info)
 
@@ -21,6 +23,10 @@ class FileSystem:
             size, used, avail = tuple(list(map(lambda x: int(x), re.findall(r" (\d+)T", info))))
             use = int(re.findall(r" (\d+)%", info)[0])
             self.nodes[(x,y)] = [size, used, avail, use]
+            self.width = max(self.width, x)
+            self.height = max(self.height, y)
+        self.width += 1
+        self.height += 1
 
     def _IsViablePair(self, n1, n2):  # (x1,y1), (x2,y2)
         if self.nodes[n1][1] == 0:  # Used
@@ -31,3 +37,22 @@ class FileSystem:
             return False
         return True
     
+    def DispToFile(self, filename):
+        s = ""
+        for j in range(self.height):
+            for i in range(self.width):
+                if i == 0 and j == 0:
+                    s += "T "
+                    continue
+                if j == 0 and i == self.width-1:
+                    s += "X "
+                    continue
+                if self.nodes[(i,j)][1] == 0:
+                    s += "O "
+                elif self.nodes[(i,j)][1] > 400:
+                    s += "| "
+                else:
+                    s += ". "
+            s += "\n"
+        with open(filename, 'w') as f:
+            f.write(s)
