d39b20a9653ac63f73909e672c446720014b26e7
Max Remzi <91099081+vaezim@users.noreply.github.com>
2024-01-17 20:47:15 -0500

    Added 2016/day23

--- 2016/day23/main.py
+++ 2016/day23/main.py
@@ -1,7 +1,7 @@
 from utils import Assembly
 
 
-with open('input', 'r+') as file:
+with open("input", "r+") as file:
     lines = file.readlines()
 
 ##### Part 1 #####
--- 2016/day23/utils.py
+++ 2016/day23/utils.py
@@ -16,6 +16,15 @@ class Assembly:
         while i < len(instructions):
             words = instructions[i]
             cmd = words[0]
+
+            # Part 2 optimization
+            if i == 5:  # skip lines 6-10
+                self.registers["a"] += self.registers["c"] * self.registers["d"]
+                self.registers["c"] = 0
+                self.registers["d"] = 0
+                i = 10
+                continue
+
             # Commands
             if cmd == "cpy":
                 if self.registers.get(words[-1]) != None:
@@ -33,20 +42,26 @@ class Assembly:
                     continue
             elif cmd == "tgl":
                 x = self._GetRegisterVal(words[1])
-                translations = {"inc": "dec", "dec": "inc", "tgl": "inc", "jnz": "cpy", "cpy": "jnz"}
+                translations = {
+                    "inc": "dec",
+                    "dec": "inc",
+                    "tgl": "inc",
+                    "jnz": "cpy",
+                    "cpy": "jnz",
+                }
                 if i + x < len(instructions):
-                    instructions[i+x][0] = translations[instructions[i+x][0]]
+                    instructions[i + x][0] = translations[instructions[i + x][0]]
             i += 1
-    
+
     def _GetRegisterVal(self, reg):
         x = self.registers.get(reg)
         if x == None:
             x = int(reg)
         return x
-    
+
 
 if __name__ == "__main__":
-    with open("test", 'r') as file:
+    with open("test", "r") as file:
         lines = file.readlines()
     ass = Assembly(lines)
     ass.Run()
